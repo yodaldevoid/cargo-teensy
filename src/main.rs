9d96a41e4903dfd4457e08532b7a56c5e70b84ca
Gabriel Smith <ga29smith@gmail.com>
2019-04-29 00:32:54 -0400

    Add clap and add basic command line support

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
+use rusty_loader::supported_mcus;
+
+use clap::{App, Arg};
+
 fn main() {
-    print_supported_mcus();
+    let matches = App::new("rusty_loader")
+        .version(option_env!("CARGO_PKG_VERSION").unwrap_or("unknown"))
+        .author("Gabriel \"yodaldevoid\" Smith <ga29smith@gmail.com>")
+        .about("A rust rewrite of teensy_loader_cli")
+        .arg(Arg::with_name("list-mcus")
+            .long("list-mcus")
+            .help("Lists supported MCUs")
+        )
+        .get_matches();
+
+    if matches.is_present("list-mcus") {
+        print_supported_mcus();
+        return;
+    }
 }
-
-
 
 fn print_supported_mcus() {
     println!("Supported MCUs are:");
