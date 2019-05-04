bbae983687d295ca5f67062b3aea6910cef089a2
Gabriel Smith <ga29smith@gmail.com>
2019-05-04 14:32:22 -0400

    Support reading an ihex file
    Does not yet program, only reads and parses the ihex file.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::fs::File;
+use std::io::Read;
 use std::thread::sleep;
 use std::time::Duration;
 
@@ -5,6 +7,8 @@ use rusty_loader::{parse_mcu, supported_mcus};
 use rusty_loader::usb::{ConnectError, Teensy};
 
 use clap::{App, Arg};
+use ihex::reader::Reader as IHexReader;
+use ihex::record::Record as IHexRecord;
 
 static mut VERBOSE: bool = false;
 
@@ -44,6 +48,9 @@ fn main() {
             .short("b")
             .help("Only boot the device, do not program")
         )
+        .arg(Arg::with_name("file")
+            .required_unless("boot-only")
+        )
         .get_matches();
 
     let mcu = match parse_mcu(matches.value_of("mcu").unwrap()) {
@@ -57,6 +64,55 @@ fn main() {
     unsafe {
         VERBOSE = matches.is_present("verbose");
     }
+
+    let file = if !matches.is_present("boot-only") {
+        let file_path = matches.value_of("file").expect("No file path though boot-only not set");
+        match File::open(file_path) {
+            Ok(mut file) => {
+                // Check the binary size
+                let mut file_str = String::new();
+                if let Err(err) = file.read_to_string(&mut file_str) {
+                    eprintln!("Failed to read \"{:?}\"", file_path);
+                    println_verbose!("Error: {}", err);
+                    std::process::exit(1);
+                }
+                let ihex_reader = IHexReader::new(&file_str);
+                let ihex_records: Result<Vec<_>, _> = ihex_reader.collect();
+                let ihex_records = match ihex_records {
+                    Ok(r) => r,
+                    Err(err) => {
+                        eprintln!("Failed to parse \"{}\" as Intel hex", file_path);
+                        println_verbose!("Error: {}", err);
+                        std::process::exit(1);
+                    }
+                };
+                let len: usize = ihex_records.iter()
+                    .map(|rec| if let IHexRecord::Data { value, .. } = rec {
+                        value.len()
+                    } else {
+                        0
+                    })
+                    .sum();
+
+                println_verbose!(
+                    "Read \"{}\": {} bytes, {:.*}% usage",
+                    file_path,
+                    len,
+                    1,
+                    len as f64 / mcu.0 as f64 * 100.0
+                );
+
+                Some(file)
+            }
+            Err(err) => {
+                eprintln!("Failed to open \"{}\"", file_path);
+                println_verbose!("Error: {}", err);
+                std::process::exit(1);
+            }
+        }
+    } else {
+        None
+    };
 
     let wait_for_device = matches.is_present("wait");
     let mut waited = false;
